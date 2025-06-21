cdc5a18e3c1019786547c15be9b8ed6acf82f14e
sami <amstudent638@gmail.com>
2025-06-21 05:45:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# typescript MERN Amazon
+
+## Lessons
+
+1. Introduction
+2. Install tools
+3. Create TypeScript React App By Vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazon</header>
+      <main></main>
+      <footer>All rights reserved.</footer>
+    </div>
+  )
+}
+
+export default App
