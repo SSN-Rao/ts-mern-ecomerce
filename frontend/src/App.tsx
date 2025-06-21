4da54aa9f46acca742adf0e89868d322547d99cd
sami <amstudent638@gmail.com>
2025-06-21 07:54:56 +0100

    list items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   return (
     <div>
       <header>TS Amazon</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                className="product-image"
+                src={product.image}
+                alt={product.name}
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved.</footer>
     </div>
   )
---
+++ frontend/src/types/product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  name: string
+  description: string
+  slug: string
+  image: string
+  price: number
+  countInStock: number
+  rating: number
+  numReviews: number
+  category: string
+  brand: string
+}
