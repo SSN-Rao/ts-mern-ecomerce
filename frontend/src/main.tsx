661620f1f5cdb7d52035f54e21b4f1342fd3f8da
sami <amstudent638@gmail.com>
2025-07-18 06:52:08 +0100

    react cotext

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,6 +15,7 @@ import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { StoreProvider } from './Store'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,11 +30,13 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <StoreProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </StoreProvider>
   </StrictMode>
 )
